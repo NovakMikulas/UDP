32a9eb5248dc3c34343d6f393aa81a6f1185b7c7
Mikuláš Novák <novakmikulas@protonmail.com>
2026-06-29 01:45:14 +0200

    docker

--- client/src/api/socket.js
+++ client/src/api/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000", {
+const socket = io(process.env.REACT_APP_SERVER_URL, {
   withCredentials: true,
   autoConnect: false,
 });
